05a14196c9b6d46ada1e6880fabb35cf16b28ae3
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2017-11-20 17:05:52 -0200

    Matlab data acquisition

--- pratica4/serial_prat4.m
+++ pratica4/serial_prat4.m
@@ -1,23 +1,39 @@
-porta = serial('COM7', 'Baudrate', 9600);
+
+porta = serial('COM24', 'Baudrate', 9600);
 if (isvalid(porta)==0)
-    exit;
+    a=2
+    
+    fclose(porta);
+    delete(porta);
+    clear porta;
 end
 
 fopen(porta);
 count=1;
-figure; hold on;
+graph = plot(0,0,'r*') 
+axis([0 20 0 200])
 
-while(count<1000)
-    temp = fscanf(porta,'%c%c%c%c%c%c%c%c'); 
-    temp = bin2dec(temp);
-    plot(count,temp);
-    hist(count) = [clock temp];
+hold on;
+temp=0;
+
+while(ishandle(graph))
+    temp0=temp;
+    aux=clock;
+    temp = fscanf(porta,'%c')
+    temp = bin2dec(temp(1:8));
+    
+  %  set(graph,'XData',count,'YData',temp)
+    hold on
+        plot([count-1 count],[temp0 temp]);
+    axis([0 count 0 200])
+%     hist(count,1:5) = clock;
+%     hist(count, 6) = temp;
     count = count+1;
+    pause(.01)
 end
 
 fclose(porta);
 delete(porta);
 clear porta;
-
 
 
