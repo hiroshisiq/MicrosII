0ee2279848f88ed0a3026cb6e097b6010e9e741d
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2017-11-20 18:12:15 -0200

    General update

--- pratica4/serial_prat4.m
+++ pratica4/serial_prat4.m
@@ -10,27 +10,44 @@ end
 
 fopen(porta);
 count=1;
-graph = plot(0,0,'r*') 
+%graph = plot(0,0,'r*') 
 axis([0 20 0 200])
 
 hold on;
-temp=0;
+temp=33;
 
-while(ishandle(graph))
+while(ishandle(1))%graph))
     temp0=temp;
     aux=clock;
     temp = fscanf(porta,'%c')
-    temp = bin2dec(temp(1:8));
+    temp = bin2dec(temp(1:8))-32;
     
   %  set(graph,'XData',count,'YData',temp)
-    hold on
+        hold on
+        subplot(4,1,1);
         plot([count-1 count],[temp0 temp]);
-    axis([0 count 0 200])
+        axis([count-50 count -30 60])
+        hold on
+        subplot(4,1,2);
+        plot([count-1 count],polyval(pol2,[temp0 temp]))
+        axis([count-50 count -30 60])
+        hold on
+        subplot(4,1,3);
+        plot([count-1 count],polyval(pol3,[temp0 temp]))
+        axis([count-50 count -30 60])
+        hold on
+        subplot(4,1,4);
+        if(temp0 >32 &&temp >32 && temp0 <107 && temp < 107)
+            plot([count-1 count], [a(temp0-32,1) a(temp-32,1)] )
+        end
+        axis([count-50 count -30 60])
 %     hist(count,1:5) = clock;
 %     hist(count, 6) = temp;
     count = count+1;
     pause(.01)
 end
+
+
 
 fclose(porta);
 delete(porta);
