94e72cf4d6df652b628ec1f38a7d8b95721d1008
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2017-11-27 18:08:11 -0200

    Minor fixes

--- pratica5/arduino/sketch_nov25b.ino
+++ pratica5/arduino/sketch_nov25b.ino
@@ -1,27 +1,27 @@
 #include <SoftwareSerial.h>
 
 // Time
-#define REJECT_M_TIME 1000
-#define REJECT_N_TIME 1000
-#define WAIT_M_TIME 2000
-#define WAIT_N_TIME 2000
-#define ACTING_TIME 500
+unsigned long REJECT_M_TIME = 4000;
+unsigned long REJECT_N_TIME = 4000;
+unsigned long WAIT_M_TIME = 6000;
+unsigned long WAIT_N_TIME = 6000;
+unsigned long ACTING_TIME = 500;
 
 // Configuration
-#define GUI true
+#define GUI false
 
 // Pinout
-const int port[8] = {4, 5 , 2 , 3 , 8 , 9 , 10 , 11};
+const byte port[8] = {4, 5 , 3 , 2 , 8 , 9 , 10 , 11};
 
-// Control
-bool opt            = false;
-bool ind            = false;
-bool cap            = false;
+// Control 
+volatile bool opt            = false;
+volatile bool ind            = false;
+volatile bool cap            = false;
 
 bool normalStart    = false;
 bool testStart      = false;
 bool rejectMetal    = false;
-bool rejectNonMetal = false;
+bool rejectNonMetal = true;
 bool left           = false;
 bool piston         = false;
 
@@ -30,36 +30,38 @@ normalState state = RESET;
 
 int lastState = 0;
 
-// Time
-unsigned int serialTime = 0;
-unsigned int nsTime = 0;
-bool nsTimerStarted = false;
+// Time 
+unsigned long serialTime; 
+unsigned long nsTime;
+bool nsTimerStarted = false; 
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial);
-
-  // Direction
-  pinMode(port[0], OUTPUT);
-  pinMode(port[1], OUTPUT);
-
+  
+  // Direction              
+  pinMode(port[0], OUTPUT); 
+  pinMode(port[1], OUTPUT); 
+                            
   // Cilinder
   pinMode(port[5], OUTPUT);
   pinMode(port[7], OUTPUT);
 
   // Sensors
-  pinMode(port[2], INPUT); // Ind
-  pinMode(port[3], INPUT); // Cap
+//  pinMode(port[2], INPUT); // Ind
+//  pinMode(port[3], INPUT); // Cap
   pinMode(port[4], INPUT); // Optical
 
   // Attach interruptions
+  pinMode(port[2], INPUT_PULLUP);
+  pinMode(port[3], INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(port[2]), handleIndSensor, FALLING);
   attachInterrupt(digitalPinToInterrupt(port[3]), handleCapSensor, FALLING);
 }
 
 void handleIndSensor() {
-  ind = true;
+  ind = true; 
 }
 
 void handleCapSensor() {
@@ -67,27 +69,27 @@ void handleCapSensor() {
 }
 
 void stopConveyor() {
-  digitalWrite(port[0], HIGH);
+  digitalWrite(port[0], HIGH); 
   digitalWrite(port[1], HIGH);
 }
 
 void turnConveyorRight() {
-  digitalWrite(port[0], LOW );
+  digitalWrite(port[0], LOW ); 
   digitalWrite(port[1], HIGH);
 }
 
 void turnConveyorLeft() {
-  digitalWrite(port[0], HIGH);
-  digitalWrite(port[1], LOW );
+  digitalWrite(port[0], HIGH); 
+  digitalWrite(port[1], LOW );  
 }
 
 void cReject() {
-  digitalWrite(port[5], HIGH);
+  digitalWrite(port[5], HIGH); 
   digitalWrite(port[7], LOW );
 }
 
 void cReturn() {
-  digitalWrite(port[5], LOW );
+  digitalWrite(port[5], LOW ); 
   digitalWrite(port[7], HIGH);
 }
 
@@ -96,7 +98,7 @@ bool getOpticalPresence() {
 }
 
 void comunicationGUI() {
-  while (Serial.available()) {
+  while (Serial.available()) {    
     // read the incoming byte:
     char incomingByte = Serial.read();
 
@@ -107,7 +109,7 @@ void comunicationGUI() {
     rejectNonMetal = (bitRead(incomingByte, 0) == 1)? true : false;
     left           = (bitRead(incomingByte, 0) == 1)? true : false;
     piston         = (bitRead(incomingByte, 0) == 1)? true : false;
-
+   
   }
 
   // Send information about the sensors
@@ -119,7 +121,7 @@ void comunicationGUI() {
 }
 
 void comunication() {
-  while (Serial.available()) {
+  while (Serial.available()) {    
     // read the incoming byte:
     char incomingByte = Serial.read();
 
@@ -129,7 +131,7 @@ void comunication() {
       testStart = false;
     } else if(incomingByte == '1') {
       normalStart = true;
-      testStart = false;
+      testStart = false;  
     } else if (incomingByte == '2') {
       normalStart = false;
       testStart = true;
@@ -141,27 +143,29 @@ void comunication() {
       left = !left;
     } else if (incomingByte == '6') {
       piston = !piston;
-    }
+    }   
   }
 
   // Send information about the sensors
   char info[10] = "000000000\0";
-  if(normalStart)          info[0] = "1";
-  if(testStart)            info[1] = "1";
-  if(rejectMetal)          info[2] = "1";
-  if(rejectNonMetal)       info[3] = "1";
-  if(left)                 info[4] = "1";
-  if(piston)               info[5] = "1";
-  if(digitalRead(port[2])) info[6] = "1"; // cap
-  if(digitalRead(port[3])) info[7] = "1"; // ind
-  if(digitalRead(port[4])) info[8] = "1"; // opt
-  Serial.println(info);
+  if(normalStart)          info[0] = "1"; 
+  if(testStart)            info[1] = "1"; 
+  if(rejectMetal)          info[2] = "1"; 
+  if(rejectNonMetal)       info[3] = "1"; 
+  if(left)                 info[4] = "1"; 
+  if(piston)               info[5] = "1"; 
+  if(cap) info[6] = "1"; // cap 
+  if(ind) info[7] = "1"; // ind
+  if(opt) info[8] = "1"; // opt
+  //Serial.println(info);
 }
 
 void normalStartStateMachine() {
   // NORMAL START STATE MACHINE
     switch(state) {
       case RESET: {
+        Serial.println("Reset");
+        
         // Conveyor on: Left
         turnConveyorLeft();
 
@@ -172,94 +176,105 @@ void normalStartStateMachine() {
         ind = false;
         cap = false;
         opt = false;
-        nsTimerStarted = false;
+        nsTimerStarted = false;         
 
         // Change Condition: Optical Sensor
         if(getOpticalPresence()) {
-          state = SELECT;
+          state = SELECT;                  
         }
-
+        
       } break;
 
       case SELECT: {
+        Serial.println("SELECT");
+        
         // Conveyor on: Right
         turnConveyorRight();
 
         // Change Condition: Capacitive Sensor
         if(cap) {
-          state = HANDLE_N;
+          state = HANDLE_N;                  
         }
 
         // Change Condition: Inductive Sensor
         if(ind) {
-          state = HANDLE_M;
+          state = HANDLE_M;                  
         }
-
+        
       } break;
 
       case HANDLE_M: {
+        Serial.println("Metal");
+                
         // Start timer
         if(nsTimerStarted == false) {
           nsTime = millis();
+          nsTimerStarted = true;
         }
+        
+        Serial.println(millis()-nsTime);
 
         // Reject or Accept material
         if(rejectMetal) {
           // Change Condition: Actuation Time
-          if(abs(nsTime - millis()) < REJECT_M_TIME) {
-            state = REJECT;
-            nsTimerStarted = false;
-          }
+          if((millis()-nsTime) > REJECT_M_TIME) {
+            state = REJECT;       
+            nsTimerStarted = false;           
+          }    
         } else {
           // Change Condition: Actuation Time
-          if(abs(nsTime - millis()) < WAIT_M_TIME) {
-            state = RESET;
+          if((millis()-nsTime) > WAIT_M_TIME) {
+            state = RESET;                  
           }
         }
-
+        
       } break;
 
       case HANDLE_N: {
+        Serial.println("NonMetal");
+        
         // Start timer
         if(nsTimerStarted == false) {
           nsTime = millis();
+          nsTimerStarted = true;
         }
 
         // Reject or Accept material
         if(rejectNonMetal) {
           // Change Condition: Actuation Time
-          if(abs(nsTime - millis()) < REJECT_N_TIME) {
-            state = REJECT;
-            nsTimerStarted = false;
-          }
+          if((millis()-nsTime) > REJECT_N_TIME) {
+            state = REJECT;       
+            nsTimerStarted = false;           
+          }    
         } else {
           // Change Condition: Actuation Time
-          if(abs(nsTime - millis()) < WAIT_N_TIME) {
-            state = RESET;
+          if((millis()-nsTime) > WAIT_N_TIME) {
+            state = RESET;                  
           }
         }
-
+        
       } break;
 
       case REJECT: {
         // Start timer
         if(nsTimerStarted == false) {
           nsTime = millis();
+          nsTimerStarted = true;
           cReject();
         }
 
         // Change Condition: Actuation Time
-        if(abs(nsTime - millis()) < ACTING_TIME) {
-          state = RESET;
+        if((millis()-nsTime) < ACTING_TIME) {
+          state = RESET;                  
         }
-
+        
       } break;
     }
 }
 
-void loop() {
+void loop() {  
   if(normalStart) {
-    normalStartStateMachine();
+    normalStartStateMachine();      
   } else {
     // Conveyor test unit
     if(testStart) {
@@ -279,13 +294,16 @@ void loop() {
       cReturn();
     }
 
-    state = RESET;
+    state = RESET;    
   }
-
+  
   // Serial comutication ativate each 100ms
-  if(abs(serialTime - millis()) > 100) {
+  if((millis() - serialTime) > 100) {
     // Send and receive info
-    GUI? comunicationGUI() : comunication();
+    GUI? comunicationGUI() : comunication();  
     serialTime = millis();
   }
+
+  
+//  Serial.println(cap);
 }
